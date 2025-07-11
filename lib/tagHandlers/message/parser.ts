2cb083836a92b7b674f73733ee4d6b495085c4c8
Adam Averay <adam@averay.com>
2025-07-11 20:58:01 +0100

    Centralise message tag parsing

--- lib/tagHandlers/message/parser.ts
+++ lib/tagHandlers/message/parser.ts
@@ -1,13 +1,14 @@
 import type { TwingExpressionNode, TwingParser, TwingTokenStream } from 'twing';
+import { createParsingError } from 'twing';
 
-import { endTagName } from '#/utils/parsing.ts';
+import { endTagName, testNextIsTag } from '#/utils/parsing.ts';
+import type { TwingIntlFragmentNode } from '#tagHandlers/fragment/node.ts';
 import type { FragmentTokenParser } from '#tagHandlers/fragment/tagHandler.ts';
 
 import type { TwingIntlMessageNode } from './node.ts';
 import createMessageNode from './node.ts';
 import type { TwingIntlMessageDescriptorNode } from './nodeDescriptor.ts';
 import createMessageDescriptorNode from './nodeDescriptor.ts';
-import parseMessageBody from './parserBody.ts';
 
 /**
  * Parses a message's opening tag. A message descriptor or ID is required. Optional values can be declared, as well as options for the formatter.
@@ -51,6 +52,30 @@ function parseOpen(
   return { descriptor, values, options };
 }
 
+function parseBody(
+  _: TwingParser,
+  stream: TwingTokenStream,
+  fragmentTag: string,
+  fragmentTokenParser: FragmentTokenParser,
+): Record<string, TwingIntlFragmentNode> {
+  stream.nextIf('WHITESPACE');
+
+  const fragmentNodes: Record<string, TwingIntlFragmentNode> = {};
+  while (testNextIsTag(stream, fragmentTag)) {
+    const { line, column } = stream.current;
+    const fragment = fragmentTokenParser(stream.current, stream);
+    const { name } = fragment.attributes;
+    if (name in fragmentNodes) {
+      throw createParsingError(`Duplicate fragment "${name}".`, { line, column }, stream.source);
+    }
+    fragmentNodes[name] = fragment;
+
+    stream.nextIf('WHITESPACE');
+  }
+
+  return fragmentNodes;
+}
+
 function parseClose(_: TwingParser, stream: TwingTokenStream, tag: string): void {
   stream.expect('TAG_START');
   stream.expect('NAME', endTagName(tag));
@@ -66,7 +91,7 @@ export default function parseMessage(
 ): TwingIntlMessageNode {
   const { line, column } = stream.current;
   const { descriptor: descriptorNode, values: valuesNode, options: optionsNode } = parseOpen(parser, stream);
-  const fragmentNodes = parseMessageBody(parser, stream, fragmentTag, fragmentTokenParser);
+  const fragmentNodes = parseBody(parser, stream, fragmentTag, fragmentTokenParser);
   parseClose(parser, stream, tag);
   return createMessageNode(tag, descriptorNode, valuesNode, optionsNode, fragmentNodes, line, column);
 }
